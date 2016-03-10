f040fcee9e33b7199d13eb3943dc903511514b05
chrissoria <csoria1@mail.ccsf.edu>
2016-03-09 23:23:34 -0800

    working table view and cell
    table view set up, getting information from cell.

---
+++ SFArtOpenings/MainViewController.swift
@@ -0,0 +1,57 @@
+//
+//  MainViewController.swift
+//  SFArtOpenings
+//
+//  Created by christopher soria on 3/9/16.
+//  Copyright © 2016 Codepath-Juan-Yousef-Chris. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 20
+    
+}
+
+func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCellWithIdentifier("ReceptionCell", forIndexPath: indexPath)
+    cell.textLabel!.text = "row\(indexPath.row)"
+    print("row\(indexPath.row)")
+    
+    
+    return cell
+}
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SFArtOpenings/ReceptionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReceptionTableViewCell.swift
+//  SFArtOpenings
+//
+//  Created by christopher soria on 3/9/16.
+//  Copyright © 2016 Codepath-Juan-Yousef-Chris. All rights reserved.
+//
+
+import UIKit
+
+class ReceptionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
