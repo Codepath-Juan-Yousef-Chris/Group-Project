d8e532039acf2111c9b36d6d8afaf298882a5387
chrissoria <csoria1@mail.ccsf.edu>
2016-03-20 19:57:05 -0700

    passing more data
    today tab fully showing data

---
+++ SFArtOpenings/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  SFArtOpenings
+//
+//  Created by christopher soria on 3/20/16.
+//  Copyright © 2016 Codepath-Juan-Yousef-Chris. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class User: NSObject {
+    
+
+}
