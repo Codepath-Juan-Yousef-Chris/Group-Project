af15929a0d83964d20b0b294fe39a78077c0d92d
chrissoria <csoria1@mail.ccsf.edu>
2016-04-16 17:40:43 -0700

    added images to uber/lyft
    added images to the uber and lyft buttons in the xib.

--- SFArtOpenings/MainViewController.swift
+++ SFArtOpenings/MainViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-//import AFNetworking
+import AFNetworking
 import Parse
 
 class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
@@ -39,8 +39,8 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         usernameBarButton.setTitle(user?.username, forState: UIControlState.Normal)
         
         
-        //refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
-        //tableView.addSubview(refreshControl)
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.addSubview(refreshControl)
         
         tableView.dataSource = self
         tableView.delegate = self
